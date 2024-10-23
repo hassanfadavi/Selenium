6befb3167efa0ff72af8b4ba8ed1782e9a440667
hassanfadavi <s.hasanfadavi@gmail.com>
2024-10-22 20:50:54 -0400

    logo

---
+++ src/test/java/tek/selenium/ClassLocator/Logo_Tekschool.java
@@ -0,0 +1,31 @@
+package tek.selenium.ClassLocator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Logo_Tekschool {
+    public static void main(String[] args) throws InterruptedException{
+
+
+        ChromeDriver chdriver=new ChromeDriver();
+        chdriver.get("https://retail.tekschool-students.com/");
+        String pagetitle=chdriver.getTitle();
+        System.out.println(pagetitle);
+        chdriver.manage().window().maximize();
+
+
+        //className (Logo)
+        By logoLocator= By.className("top-nav__logo");
+        WebElement logoElement =chdriver.findElement(logoLocator);
+        String logo=logoElement.getText();
+        System.out.println(logo);
+
+        Thread.sleep(5000);
+        chdriver.quit();
+        
+        
+        
+        
+    }
+}
