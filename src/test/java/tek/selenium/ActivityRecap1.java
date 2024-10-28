64edd016c2d2df921c315d2b9a39fba67efc3a5b
hassanfadavi <s.hasanfadavi@gmail.com>
2024-10-28 19:38:14 -0400

    modified

--- src/test/java/tek/selenium/ActivityRecap1.java
+++ src/test/java/tek/selenium/ActivityRecap1.java
@@ -16,33 +16,31 @@ public class ActivityRecap1 {
         Thread.sleep(2000);
 
        //input TV
-        By inputSearch = By.xpath(" /html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/input");
+        By inputSearch = By.xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/input");
         WebElement inputElement= chdriver.findElement(inputSearch);
         inputElement.sendKeys("TV");
         Thread.sleep(1000);
 
         //search Button
-        By  SearchLocator = By.xpath(" /html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button");
+        By  SearchLocator = By.xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button");
         WebElement searchElement= chdriver.findElement(SearchLocator);
         searchElement.click();
         Thread.sleep(1000);
 
 
          //SELECT TV
-        By  tvLocator = By.xpath(" /html/body/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/p ");
+        By  tvLocator = By.xpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/p");
         WebElement tvElement= chdriver.findElement(tvLocator);
         tvElement.click();
         Thread.sleep(1000);
 
 
         //print title
-        By  titlePrint = By.xpath(" /html/body/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/h1 ");
-        WebElement titleement= chdriver.findElement(titlePrint);
-        String text=titleement.getText();
+        By  titlePrint = By.xpath("/html/body/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/h1");
+        WebElement titleEment= chdriver.findElement(titlePrint);
+        String text=titleEment.getText();
         System.out.println(text);
         Thread.sleep(1000);
-
-
 
 
         chdriver.quit();
