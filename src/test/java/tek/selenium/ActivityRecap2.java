71139a4dd38ffa2a3c38c1c3adab64ff21392fc7
hassanfadavi <s.hasanfadavi@gmail.com>
2024-10-28 20:25:59 -0400

    activity2

---
+++ src/test/java/tek/selenium/ActivityRecap2.java
@@ -0,0 +1,56 @@
+package tek.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ActivityRecap2 {
+    public static void main(String[] args) throws InterruptedException {
+
+             /*
+    In Retail App https://retail.tekschool-students.com/
+    Search for Keyboard  and Click on First product displayed
+    Then print the product title on the next page.
+
+     */
+
+
+        //open Chrome
+        ChromeDriver chdriver=new ChromeDriver();
+        chdriver.get("https://retail.tekschool-students.com/");
+        chdriver.manage().window().maximize();
+        String pageTitle=chdriver.getTitle();
+        System.out.println(pageTitle);
+        Thread.sleep(2000);
+
+        //input keyboard
+        By inputSearch = By.xpath("//input[@id='searchInput']");
+        WebElement inputElement= chdriver.findElement(inputSearch);
+        inputElement.sendKeys("keyboard");
+        Thread.sleep(1000);
+
+        //search Button
+        By  SearchLocator = By.xpath("//button[@id='searchBtn']");
+        WebElement searchElement= chdriver.findElement(SearchLocator);
+        searchElement.click();
+        Thread.sleep(1000);
+
+
+        //SELECT keyboard
+        By  keyboardLocator = By.xpath("//div[1][@class='products__layout']/img");
+        WebElement keyboardElement= chdriver.findElement(keyboardLocator);
+        keyboardElement.click();
+        Thread.sleep(1000);
+
+
+        //print title
+        By  titlePrint = By.xpath("//h1[@class='product__name']");
+        WebElement titleEment= chdriver.findElement(titlePrint);
+        String text=titleEment.getText();
+        System.out.println(text);
+        Thread.sleep(1000);
+
+
+        chdriver.quit();
+    }
+}
